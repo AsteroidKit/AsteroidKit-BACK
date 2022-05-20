a08a93172f82c7c1a46c8ab5dca5bc44486ac040
Rodrigo borba <rodrigo@chainengine.xyz>
2022-05-20 19:45:43 +0200

    feat: 🎸 Initial commit

---
+++ src/AsteroidKitProvider.tsx
@@ -0,0 +1,41 @@
+import React, { FC, ReactNode } from 'react';
+
+import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
+
+import '@rainbow-me/rainbowkit/styles.css';
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, polygon],
+  [
+    alchemyProvider({
+      // This is Alchemy's default API key.
+      // You can get your own at https://dashboard.alchemyapi.io
+      apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
+    }),
+    publicProvider(),
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'My RainbowKit App',
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+const AsteroidKitProvider: FC<{ children: ReactNode }> = ({ children }) => (
+  <WagmiConfig client={wagmiClient}>
+    <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+  </WagmiConfig>
+);
+
+export { AsteroidKitProvider };
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import * as rainbowkit from '@rainbow-me/rainbowkit';
+import * as wagmi from 'wagmi';
+import { AsteroidKitProvider } from './AsteroidKitProvider';
+
+export { AsteroidKitProvider, rainbowkit, wagmi };
