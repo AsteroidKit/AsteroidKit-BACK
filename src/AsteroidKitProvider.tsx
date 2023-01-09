562b597f6b0b5890423c0f96d6f6435e64e78898
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2023-01-09 11:06:58 -0300

    chore: remove unused networks

--- src/AsteroidKitProvider.tsx
+++ src/AsteroidKitProvider.tsx
@@ -2,7 +2,7 @@ import React, { FC, ReactNode } from 'react';
 
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { mainnet, polygon } from 'wagmi/chains';
 
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
@@ -10,7 +10,7 @@ import { publicProvider } from 'wagmi/providers/public';
 import '@rainbow-me/rainbowkit/styles.css';
 
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum, polygon],
+  [mainnet, polygon],
   [
     alchemyProvider({
       // This is Alchemy's default API key.
