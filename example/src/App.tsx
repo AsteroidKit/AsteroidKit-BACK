593cae7823b079a895ee0adee875e7b5771f21ec
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2023-01-28 00:05:03 -0300

    feat: change example app to new interface

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,29 +1,16 @@
 import { AsteroidKitProvider, ConnectButton, createClient } from 'asteroidkit';
+import 'asteroidkit/styles.css';
 import { WagmiConfig } from 'wagmi';
 
 import './App.css';
 
-const client = createClient({
-  appId: 'YOUR_APP_ID',
-  social: true,
-  wallets: [
-    { name: 'metamask', enabled: true },
-    { name: 'coinbase', enabled: true },
-    { name: 'ledger', enabled: true },
-    { name: 'argent', enabled: false },
-  ],
-});
+const client = createClient();
 
 function App() {
   return (
     <div className="App">
       <WagmiConfig client={client}>
-        <AsteroidKitProvider
-          config={{
-            enableSiwe: true,
-            enableSocial: true,
-          }}
-        >
+        <AsteroidKitProvider appId="testapp">
           <ConnectButton />
         </AsteroidKitProvider>
       </WagmiConfig>
