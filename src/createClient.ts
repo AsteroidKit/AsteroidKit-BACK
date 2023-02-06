e96a7ba167ce433b8168134b158243123c642c11
Rodrigo José Borba Fernandes <rodrigoj.borba@gmail.com>
2023-02-06 16:25:18 -0300

    fix: add mumbai network to options

--- src/createClient.ts
+++ src/createClient.ts
@@ -5,14 +5,20 @@ import { metaMaskWallet } from 'asteroidkit-rk/wallets';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
 
-import { avalanche, mainnet, optimism, polygon } from 'wagmi/chains';
+import {
+  avalanche,
+  mainnet,
+  optimism,
+  polygon,
+  polygonMumbai,
+} from 'wagmi/chains';
 
 import { configureChains, createClient as wagmiCreateClient } from 'wagmi';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createClient = () => {
   const { chains, provider } = configureChains(
-    [mainnet, optimism, avalanche, polygon],
+    [mainnet, optimism, avalanche, polygon, polygonMumbai],
     [
       alchemyProvider({
         // This is Alchemy's default API key.
